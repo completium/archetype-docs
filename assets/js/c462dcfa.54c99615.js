7cd2742d57b9d962478e53e34a4786b11b1af656
Benoit Rognier <benoit.rognier@edukera.com>
2022-06-07 18:32:31 +0200

    Deploy website - based on 1c97f15d3a56c4a833cb66f41bcc1ed1aa7368d7

---
+++ assets/js/c462dcfa.54c99615.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkarchetype_docs=self.webpackChunkarchetype_docs||[]).push([[2466],{2709:function(e){e.exports=JSON.parse('{"permalink":"/archetype-docs/blog/tags/version","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
